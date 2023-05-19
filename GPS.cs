297c3b412c02e7caf7db1d02ff236f5468a638ba
Dung <dungbui687@gmail.com>
2023-05-19 18:18:48 +0300

    GPS data reader

---
+++ GPS.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+
+namespace GPS
+{
+    public class GPS
+    {
+        public double latitude { get; set; }
+        public double longitude { get; set; }
+        public string gpsTime { get; set; }
+        public int speed { get; set; }
+        public int angle { get; set; }
+        public int altitude { get; set; }
+        public int satellites { get; set; }
+        public GPS(double latitude, double longitude, string gpsTime, int speed, int angle, int altitude, int satellites)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.gpsTime = gpsTime;
+            this.speed = speed;
+            this.angle = angle;
+            this.altitude = altitude;
+            this.satellites = satellites;
+        }
+        public GPS()
+        {
+            this.latitude = 0;
+            this.longitude = 0;
+            this.gpsTime = "";
+            this.speed = 0;
+            this.angle = 0;
+            this.altitude = 0;
+            this.satellites = 0;
+        }
+    }
+
+    public class GPSDataReader
+    {
+        public static List<GPS> readGPSData(string path)
+        {
+            List<GPS>? jsonData = new List<GPS>();
+            try
+            {
+                string json = File.ReadAllText(path);
+                jsonData = JsonConvert.DeserializeObject<List<GPS>>(json);
+            }
+            catch
+            {
+                Console.WriteLine("ERROR: Can't read the file on " + path);
+            }
+            return jsonData;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+using GPS;
+
+public class Program
+{
+    public static void Main()
+    {
+        List<GPS.GPS> data = GPSDataReader.readGPSData("2019-07.json");
+        foreach(GPS.GPS gps in data)
+        {
+            Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", gps.latitude, gps.longitude, gps.gpsTime, gps.speed, gps.angle, gps.altitude, gps.satellites);
+        }
+    }
+}
