be562798f442393269b50f973587cbc63ce0e2ea
Dung <dungbui687@gmail.com>
2023-05-19 19:54:30 +0300

    fix json, add csv read

--- GPS.cs
+++ GPS.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+﻿using System.Data;
 
 namespace GPS
 {
@@ -35,19 +35,97 @@ namespace GPS
 
     public class GPSDataReader
     {
-        public static List<GPS> readGPSData(string path)
+        public static List<GPS> readGPSJson(string path)
         {
             List<GPS>? jsonData = new List<GPS>();
             try
             {
+                List<string> data = new List<string>();
+                bool inBrackets = false;
+                bool inQuotes = false;
+                int index = 0;
                 string json = File.ReadAllText(path);
-                jsonData = JsonConvert.DeserializeObject<List<GPS>>(json);
+                foreach (char c in json)
+                {
+                    //CHECK IF THE READ SYMBOL IS IN QUOTES
+                    if (c == '"')
+                    {
+                        if (inQuotes) inQuotes = false; inQuotes = true;
+                    }
+                    //IGNORE UNNECESSARY SYMBOLS
+                    if (c != ':' && c != '[' && c != ']' && c != '{' && c != '}' && c != '"' && c != ',' && data.Count > 0)
+                    {
+                        data[index] += c;
+                    }
+                    //DON'T IGNORE ':' SYMBOL FOR DIFFER
+                    if (c == ':' && inQuotes)
+                    {
+                        data[index] += c;
+                    }
+                    if(c == '{')
+                    {
+                        data.Add("");
+                        inBrackets = true;
+                    }
+                    if ((c == ':' && !inQuotes) || (c == ',' && inBrackets))
+                    {
+                        data.Add("");
+                        index++;
+                    }
+                    if(c == '}')
+                    {
+                        inBrackets = false;
+
+                        //REMOVE JSON KEY VALUES (e.g. Latitude:)
+                        for(int i = 0; i < data.Count; i++)
+                        {
+                            data[i] = data[i].Substring(data[i].IndexOf(':') + 1);
+                        }
+
+                        GPS temp = new GPS(Convert.ToDouble(data[0]), Convert.ToDouble(data[1]), data[2], Convert.ToInt32(data[3]), Convert.ToInt32(data[4]), Convert.ToInt32(data[5]), Convert.ToInt32(data[6]));
+                        jsonData.Add(temp);
+
+                        data.Clear();
+                        index = 0;
+                    }
+                }
             }
             catch
             {
                 Console.WriteLine("ERROR: Can't read the file on " + path);
             }
             return jsonData;
+        }
+        public static List<GPS> readGPSCsv(string path)
+        {
+            List<GPS>? csvData = new List<GPS>();
+            try
+            {
+                IEnumerable<string> csv = File.ReadLines(path);
+                foreach(var line in csv)
+                {
+                    //STORING EVERY COLUMN OF A ROW IN CSV FILE
+                    List<string> data = new List<string>();
+                    int index = 0;
+                    data.Add("");
+                    foreach(char c in line)
+                    {
+                        if (c != ',') data[index] += c;
+                        if (c == ',')
+                        {
+                            data.Add("");
+                            index++;
+                        }
+                    }
+                    GPS temp = new GPS(Convert.ToDouble(data[0]), Convert.ToDouble(data[1]), data[2], Convert.ToInt32(data[3]), Convert.ToInt32(data[4]), Convert.ToInt32(data[5]), Convert.ToInt32(data[6]));
+                    csvData.Add(temp);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("ERROR: Can't read the file on " + path);
+            }
+            return csvData;
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,7 +5,10 @@ public class Program
 {
     public static void Main()
     {
-        List<GPS.GPS> data = GPSDataReader.readGPSData("2019-07.json");
+        List<GPS.GPS> data = GPSDataReader.readGPSJson("2019-07.json");
+        /*
+        List<GPS.GPS> data = GPSDataReader.readGPSCsv("2019-08.csv");
+        */
         foreach(GPS.GPS gps in data)
         {
             Console.WriteLine("{0} {1} {2} {3} {4} {5} {6}", gps.latitude, gps.longitude, gps.gpsTime, gps.speed, gps.angle, gps.altitude, gps.satellites);
